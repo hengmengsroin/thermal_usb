f60f6895963ec49591f09cc97270d8995482c9b9
Mengsroin <hengmengsroin@gmail.com>
2024-12-15 16:50:25 +0700

    Add WebSerialReceiptPrinter class and enhance USB connection handling

--- assets/lib/usb_connector.js
+++ assets/lib/usb_connector.js
@@ -1,54 +1,39 @@
 // usb_connector.js
-if ('usb' in navigator) {
-  console.log('WebUSB API is supported in this browser.');
-  async function connectUSBDevice(vendorId) {
+if ("usb" in navigator) {
+  console.log("WebUSB API is supported in this browser.");
+  const userAgent = navigator.userAgent.toLowerCase();
+  let receiptPrinter = new WebSerialReceiptPrinter();
+
+  async function connectUSBDevice() {
     try {
-       const filters = [];
-      if (vendorId) {
-        filters.push({ vendorId: vendorId });
-      }
-
-      navigator.usb.getDevices().then((devices) => {
-        devices.forEach((device) => {
-          console.log(device.productName); // "Arduino Micro"
-          console.log(device.manufacturerName); // "Arduino LLC"
-        });
-      });
-      
-      const device = await navigator.usb.requestDevice({
-        filters:filters // Replace with your device's vendorId
+      receiptPrinter.addEventListener("connected", (device) => {
+        console.log({ device });
       });
 
-      console.log(`Device selected: ${device.productName}`);
-      
-      await device.open();
-      console.log('Device opened.');
-
-      if (device.configuration === null) {
-        await device.selectConfiguration(1);
-        console.log('Configuration selected.');
-      }
-
-      await device.claimInterface(0);
-      console.log('Interface claimed.');
-
-      await device.controlTransferOut({
-        requestType: 'vendor',
-        recipient: 'device',
-        request: 0x01,
-        value: 0x0001,
-        index: 0x0000
+      receiptPrinter.addEventListener("disconnect", () => {
+        console.log("Disconnected");
       });
-      console.log('Control transfer sent.');
+
+      receiptPrinter.connect();
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   }
 
+  function print(data) {
+    console.log("Print test");
+    console.log({ data });
+    /* Print the receipt */
+    receiptPrinter.print(data);
+  }
   window.connectUSBDevice = connectUSBDevice; // Expose function globally
+  window.print = print; // Expose function globally
 } else {
-  console.log('WebUSB API is not supported in this browser.');
+  console.log("WebUSB API is not supported in this browser.");
   window.connectUSBDevice = () => {
-    console.error('WebUSB API is not supported in this browser.');
+    console.error("WebUSB API is not supported in this browser.");
+  };
+  window.print = () => {
+    console.error("WebUSB API is not supported in this browser.");
   };
 }
